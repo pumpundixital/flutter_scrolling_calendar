6ca6ad2abbe70bf63db78c4150915cf6a63fba4d
Carlos <carlos@pumpunhost.com>
2019-09-19 17:03:15 +0200

    Test

--- lib/utils/screen_sizes.dart
+++ lib/utils/screen_sizes.dart
@@ -21,7 +21,7 @@ ScreenSizes screenSize(BuildContext context) {
 
 /// Gets the size of the day number widget.
 double getDayNumberSize(BuildContext context) {
-  return screenSize(context) == ScreenSizes.small ? 12.0 : 15.0;
+  return screenSize(context) == ScreenSizes.small ? 12.0 : 25.0;
 }
 
 /// Gets the vertical space that one month view takes up.
