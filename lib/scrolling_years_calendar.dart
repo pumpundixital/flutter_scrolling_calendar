5fb3a4da5ddbeadbc441083110ed0c5ef028cc0f
Carlos <carlos@pumpunhost.com>
2019-10-10 08:42:42 +0200

    Maybe

--- lib/scrolling_years_calendar.dart
+++ lib/scrolling_years_calendar.dart
@@ -13,6 +13,7 @@ class ScrollingYearsCalendar extends StatefulWidget {
     this.todayColor,
     this.monthNames,
     this.onMonthTap,
+    this.key,
   })  : assert(context != null),
         assert(initialDate != null),
         assert(firstDate != null),
@@ -33,6 +34,7 @@ class ScrollingYearsCalendar extends StatefulWidget {
   final Color todayColor;
   final List<String> monthNames;
   final Function onMonthTap;
+  final GlobalKey key;
 
   @override
   _ScrollingYearsCalendarState createState() => _ScrollingYearsCalendarState();
@@ -47,6 +49,7 @@ class _ScrollingYearsCalendarState extends State<ScrollingYearsCalendar> {
       todayColor: widget.todayColor,
       monthNames: widget.monthNames,
       onMonthTap: widget.onMonthTap,
+      key: widget.key,
     );
   }
 
